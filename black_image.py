5c6ac86f37d147e637a4aad4b8fff67628efa4f3
toshinakatsu <toshinakatsu5827@gmail.com>
2021-10-31 20:11:31 +0900

    黒画像を生成する

--- black_image.py
+++ black_image.py
@@ -3,7 +3,7 @@ import cv2
 # 画像の読み込み
 cv_img = cv2.imread('lena.jpg')
 
-# 黒画像を生成する
+# 黒画像を生成して表示する
 black_img = cv_img * 0
 cv2.imshow("black_img", black_img)
 cv2.waitKey(0) & amp; 0xFF == ord('q')
